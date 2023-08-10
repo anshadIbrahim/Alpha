956a39f801570c388e34fafad4706fdccc84a43f
anshadIbrahim <141921409+anshadIbrahim@users.noreply.github.com>
2023-08-10 16:20:27 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+function sendMail(){
+    var params={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value
+    };
+
+const serviceId="service_xvw93ny";
+const templateId="template_qvfy7bm";
+emailjs.send(serviceId,templateId,params)
+.then(
+    res=>{
+        document.getElementById("name").value="";
+        document.getElementById("email").value="";
+        document.getElementById("message").value="";
+        console.log(res);
+        alert("your message send successfully")
+    }
+)
+.catch(err=>console.log(err));
+}
